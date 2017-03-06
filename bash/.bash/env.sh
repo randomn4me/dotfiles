9f44db8836401b268a98a0da050d647769aee15e
Philipp Jonczyk <p.jonczyk@posteo.de>
2017-03-06 12:16:39 +0100

    move entries from profile to env

--- bash/.bash/env.sh
+++ bash/.bash/env.sh
@@ -2,6 +2,9 @@ export EDITOR="vim"
 export VISUAL="vim"
 export PAGER="less"
 
+export PATH="$PATH:$HOME/bin"
+export LC_LANG="en_US.UTF-8"
+
 export XDG_CONFIG_HOME="${HOME}/.config"
 
 export MAIL="$HOME/var/mail/Inbox"
--- bash/.profile
+++ bash/.profile
@@ -1,8 +1,1 @@
-export EDITOR="vim"
-export VISUAL="vim"
-export PAGER="less"
-
-export PATH="$PATH:$HOME/bin"
-export LC_LANG="en_US.UTF-8"
-
 test -f "$HOME/.bashrc" && source "$HOME/.bashrc"
