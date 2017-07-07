f2f488b0a5831ba92e8198d172bdf316dff2f4fc
Philipp Jonczyk <p.jonczyk@posteo.de>
2017-07-07 21:34:50 +0200

    changed alias

--- bash/.bash/70_aliases.sh
+++ bash/.bash/70_aliases.sh
@@ -14,11 +14,13 @@ alias "uni"="$(curterm) && cd $(curterm)"
 alias "paper"="test ! -z $PAPER_DIR && cd $PAPER_DIR"
 
 # useful
-alias musicdl='youtube-dl -x --audio-format mp3'
+alias mp3dl='youtube-dl -x --audio-format mp3'
 alias rm-orphans='test -z "`pacaur -Qdtq`" && echo "no orphan" || pacaur -R $(pacaur -Qdtq)'
 alias wanip="curl -s https://4.ifcfg.me/ | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b'"
 alias kernelcodestyle="curl -s https://www.kernel.org/doc/Documentation/process/coding-style.rst | less"
 alias weather="weather Darmstadt"
+alias csv='column -t -s\;'
+alias disks='echo "╓───── m o u n t . p o i n t s"; echo "╙────────────────────────────────────── ─ ─ "; lsblk -a; echo ""; echo "╓───── d i s k . u s a g e"; echo "╙────────────────────────────────────── ─ ─ "; df -h;'
 
 # shorter
 alias open="xdg-open"
@@ -38,7 +40,6 @@ alias future="toilet -t -f future"
 alias ascii="toilet -t -f 3d"
 alias rusto="toilet -t -f rusto"
 alias rustofat="toilet -t -f rustofat"
-alias disks='echo "╓───── m o u n t . p o i n t s"; echo "╙────────────────────────────────────── ─ ─ "; lsblk -a; echo ""; echo "╓───── d i s k . u s a g e"; echo "╙────────────────────────────────────── ─ ─ "; df -h;'
 alias antivirus="sudo freshclam && sudo clamscan"
 alias rice="curl -L git.io/rice; tput sgr0"
-alias csv='column -t -s\;'
+alias music='tmx r'
