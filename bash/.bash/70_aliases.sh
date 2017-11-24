2cd613224304eeacb0ada63b5f3cf1669eedf0b6
Philipp Jonczyk <p.jonczyk@posteo.de>
2017-11-24 13:41:32 +0100

    changed uni alias

--- bash/.bash/70_aliases.sh
+++ bash/.bash/70_aliases.sh
@@ -10,7 +10,7 @@ alias mv='mv -i'
 
 # cd
 alias ".."="cd ../"
-alias "uni"="which curterm && [ -d $(curterm) ] && cd $(curterm)"
+alias "uni"="cd `curterm`"
 alias "paper"="test ! -z $PAPER_DIR && cd $PAPER_DIR"
 
 # useful
