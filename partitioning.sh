b9277be25cdbaaea24258974222645c86bea92c8
Philipp Kühn <git@audacis.net>
2024-02-22 10:24:48 +0100

    correcting luks name

--- partitioning.sh
+++ partitioning.sh
@@ -13,17 +13,20 @@ echo "make filesystem for /boot on ${ROOT}p1"
 mkfs.fat -F 32 ${ROOT}p1
 
 # setup luks
-CRYPTNAME=cryptroot
+CRYPTNAME=luks
+echo "####################"
 echo "setting up rest on ${ROOT}p2"
-echo ""
-echo "setting up luks"
+echo "setting up with cryptname ${CRYPTNAME}"
+echo "####################"
 
 cryptsetup luksFormat ${ROOT}p2
 cryptsetup luksOpen ${ROOT}p2 $CRYPTNAME
 
 CRYPTROOT=/dev/mapper/$CRYPTNAME
 
+echo "####################"
 echo "generate btrfs subvolumes"
+echo "####################"
 
 mkfs.btrfs ${CRYPTROOT}
 
@@ -38,7 +41,9 @@ btrfs sub create /mnt/@snapshots
 
 umount /mnt
 
+echo "####################"
 echo "mount btrfs subvolumes from $CRYPTROOT to /mnt"
+echo "####################"
 
 mount -o noatime,nodiratime,compress=zstd,space_cache=v2,ssd,subvol=@ $CRYPTROOT /mnt
 
@@ -50,7 +55,9 @@ mount -o noatime,nodiratime,compress=zstd,space_cache=v2,ssd,subvolid=5 $CRYPTRO
 
 mount /dev/${ROOT}p1
 
+echo "####################"
 echo "setting up swap"
+echo "####################"
 
 pushd /mnt/btrfs/@swap
 truncate -s 0 ./swapfile
@@ -63,6 +70,8 @@ swapon ./swapfile
 popd
 
 
+echo "####################"
 echo "installing packages in /mnt"
+echo "####################"
 pacstrap /mnt linux linux-firmware base btrfs-progs intel-ucode nvim
 
