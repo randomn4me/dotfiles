88920451ff2878689dfbc86c8757c22296e6b34d
Philipp Kühn <git@audacis.net>
2024-12-28 20:16:15 +0100

    easier

--- nvim/nvim/lua/r4ndom/lazy/trouble.lua
+++ nvim/nvim/lua/r4ndom/lazy/trouble.lua
@@ -1,37 +1,18 @@
 return {
-  "folke/trouble.nvim",
-  opts = {}, -- for default options, refer to the configuration section for custom setup.
-  cmd = "Trouble",
-  keys = {
-    {
-      "<leader>xx",
-      "<cmd>Trouble diagnostics toggle<cr>",
-      desc = "Diagnostics (Trouble)",
-    },
-    {
-      "<leader>xX",
-      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-      desc = "Buffer Diagnostics (Trouble)",
-    },
-    {
-      "<leader>cs",
-      "<cmd>Trouble symbols toggle focus=false<cr>",
-      desc = "Symbols (Trouble)",
-    },
-    {
-      "<leader>cl",
-      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-      desc = "LSP Definitions / references / ... (Trouble)",
-    },
-    {
-      "<leader>xL",
-      "<cmd>Trouble loclist toggle<cr>",
-      desc = "Location List (Trouble)",
-    },
-    {
-      "<leader>xQ",
-      "<cmd>Trouble qflist toggle<cr>",
-      desc = "Quickfix List (Trouble)",
-    },
-  },
+	"folke/trouble.nvim",
+	config = function()
+		require("trouble").setup({
+			icons = false,
+		})
+
+		vim.keymap.set("n", "<leader>tt", function()
+			require("trouble").toggle()
+		end)
+		vim.keymap.set("n", "[t", function()
+			require("trouble").next({ skip_groups = true, jump = true })
+		end)
+		vim.keymap.set("n", "]t", function()
+			require("trouble").previous({ skip_groups = true, jump = true })
+		end)
+	end,
 }
