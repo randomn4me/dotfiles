068ee0967c35b5e13fca0988f8077637e2c07de4
Philipp Kühn <git@audacis.net>
2025-07-25 20:22:59 +0200

    macos changes

--- zsh/.zsh/10_macos.sh
+++ zsh/.zsh/10_macos.sh
@@ -1,6 +1,16 @@
 export PATH="/opt/homebrew/bin:$PATH"
 export PATH="/opt/homebrew/opt/task@2/bin:$PATH"
+export PATH="/usr/local/texlive/2025/bin/universal-darwin:$PATH"
 
+export XDG_DESKTOP_DIR="$HOME/Desktop"
+export XDG_DOCUMENTS_DIR="$HOME/Documents"
+export XDG_DOWNLOAD_DIR="$HOME/Downloads"
+export XDG_MUSIC_DIR="$HOME/Music"
+export XDG_PICTURES_DIR="$HOME/Pictures"
+export XDG_PUBLICSHARE_DIR="$HOME/Public"
+export XDG_VIDEOS_DIR="$HOME/Movies"
+
+alias claude="/Users/pkuehn/.claude/local/claude"
 
 if type brew &>/dev/null; then
     eval "$(brew shellenv)"
@@ -12,5 +22,7 @@ fpath+=/opt/homebrew/share/zsh/site-functions
 if which task &>/dev/null; then
     fpath+=/opt/homebrew/opt/task@2/share/zsh/site-functions 
 fi
+
+eval "$(direnv hook zsh)"
 
 export JAVA_HOME=$(/usr/libexec/java_home)
